b7b27474185909fe91ba588042f5076c7ab0d5f4
James-Do9 <dojames3@yahoo.com>
2021-01-11 23:38:21 +0000

    Update

--- exercises/13.4-Making_HTML_with_filter_and_maP/app.py
+++ exercises/13.4-Making_HTML_with_filter_and_maP/app.py
@@ -9,14 +9,11 @@ all_colors = [
 ]
 
 #Your code go here:
-
-#def generate_li(x):
-    #pass
-def filter_colors(x):
+def filter_colors(x): #Filters an array and only return colors with "sexy": True
     for content in x:
         if x["sexy"] == True:
             return x["label"]
-def generate_li(x):
+def generate_li(x): #Generates a <li> color name </li> for each index in sexy_colors
     for i in x:
         return "<li>" + x[i] + "</li>"
 
@@ -24,6 +21,6 @@ sexy_colors = list(filter(filter_colors, all_colors))
 generate_html = list(map(generate_li, sexy_colors))
 
 new_html = ""
-for i in range(0, len(generate_html)):
+for i in range(0, len(generate_html)): #Adds all of the <li> color name </li> generated from the mapping funciton
     new_html = new_html + generate_html[i]
 print(new_html)
