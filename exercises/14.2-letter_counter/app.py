e87bc1c7bfa7f16321af4fc4c5201a060b1da9bc
James-Do9 <dojames3@yahoo.com>
2021-01-12 00:23:09 +0000

    Update

--- exercises/14.2-letter_counter/app.py
+++ exercises/14.2-letter_counter/app.py
@@ -1,7 +1,10 @@
+from string import ascii_lowercase
+
 par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 
 counts = {}
 #your code go here:
-
+for i in ascii_lowercase:
+    counts.update({i : str(par.count(i))})
 print(counts)
 
